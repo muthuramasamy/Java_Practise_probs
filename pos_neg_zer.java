0f5a8e7655b6baffaa88a74e07400341262276dd
Muthu Ramasamy <40240197+muthuramasamy@users.noreply.github.com>
2019-08-09 09:37:11 +0530

    Create pos_neg_zer.java

---
+++ pos_neg_zer.java
@@ -0,0 +1,15 @@
+public class pnz{
+
+     public static void main(String []args){
+         int n=0;
+         Scanner n=new Scanner(System.in);
+         if (n>0){
+            System.out.println("The Number is Postive")
+         }
+         else if (n==0){
+            System.out.println("The Number is Zero")
+         }
+         else{
+            System.out.println("The Number is Negative");
+     }}
+}
