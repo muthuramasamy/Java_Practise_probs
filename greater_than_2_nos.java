d4a94b594a43c2c656c023e3c52b3806242f49e5
Muthu Ramasamy <40240197+muthuramasamy@users.noreply.github.com>
2019-08-09 10:10:21 +0530

    Create greater_than_2_nos.java

---
+++ greater_than_2_nos.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class gretw{
+public static void main(String a[]){
+int x=0,y=0;
+Scanner n=new Scanner(System.in);
+Scanner m=new Scanner(System.in);
+x=n.nextInt();
+y=m.nextInt();
+if ( x > y)
+{
+System.out.println(x+" is greater than" +y);
+}
+else
+{
+System.out.println(y +" is greater than" +x);
+}
+}
+}
